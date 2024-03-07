07be89ac4346ca2d4a7537d2408b8bf054787fd1
M.Talha Tahir <hamzatalhabwp4@gmail.com>
2024-03-07 01:19:53 -0800

    Learned Constant and Variable

---
+++ Constants/constants.js
@@ -0,0 +1,23 @@
+// const MyBirthday= '9/14/2009';
+// MyBirthday = '2990';
+// alert(MyBirthday);
+// console.log(MyBirthday);
+
+// const Talha = 'Talha';
+// const Taha = 'Taha';
+// const Umer = 'Umer';
+// const Rana = 'Rana';
+
+// let name = (Umer);
+// alert(name);
+
+// Test of variable and constant
+
+// let name= 'Jhon'
+// let admin;
+// admin = name;
+// alert(admin);
+
+const ourPlanetName= 'Earth';
+let currentUserName= 'Talha';
+alert(currentUserName);
---
+++ Variables/variables.js
@@ -0,0 +1,41 @@
+// let user = 'Hello World';
+// user = 'Jhon';
+// alert(user);
+
+// let user= 'Jhon';
+// let age;
+// age = user;
+// alert(age);
+// alert(user);
+
+// let message = 'Hello World!';
+//  message = 'Hello World-2!';
+// console.log(message);
+
+// Valid Names:
+
+// let n_1= 2;
+// let n_2= 4;
+// alert(n_1 + n_2);
+
+// Invalid Names:
+
+// let ^-1= 2;
+// let 2_test = 3;
+// alert( ^-1 + 2_test);
+
+// Case Matters
+
+// let apple= 'This is apple';
+// let Apple= 'This is Apple number 2';
+// let APPLE= 'This is Apple number 3';
+// alert(apple);
+// alert(Apple);
+// alert(APPLE);
+
+// Use strict
+// "use strict";
+
+// num = 5;
+// alert(num);
+
