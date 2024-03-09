f8e7247b97d2eb93f19ba96fe4f3e6fde3556c23
M.Talha Tahir <hamzatalhabwp4@gmail.com>
2024-03-09 11:57:41 -0800

    Learned objects and build-in Objects.

---
+++ Objects/built-in-objects.js
@@ -0,0 +1,47 @@
+// Time and intervals
+// setTimeout(function(){
+//     console.log("Finished!")
+// }
+// , 1000)
+
+// setInterval(function(){
+//     console.log("Ping!")
+// },500 );
+// const interval = setInterval(function(){
+//     console.log("Ping!")
+// },500 );
+
+// setTimeout(function(){
+//   clearInterval(interval);
+// }, 2900);
+
+
+// Formats and Values
+
+// let a = 20;
+// console.log(a, toString());
+// let a = 20;
+// let str = a.toString();
+// console.log(str);
+// console.log(typeof str)
+
+// let a = 10.4;
+// console.log(a.toFixed());
+// str = a.toString();
+// console.log(typeof str);
+
+
+// String Functions
+
+
+// let string = "Any String here!";
+// console.log(string.split(' '));
+
+
+// Math Functions
+
+// let a = -3.2;
+// console.log(Math.random());
+let rnd = Math.ceil(Math.random() * 100) + 1;
+console.log(rnd);
+
---
+++ Objects/objects.js
@@ -0,0 +1,52 @@
+// object = A collection of related properties and/or methods
+//                Can represent real world objects (people, products, places)
+//                object = {key:value,
+//                                 function()}
+
+
+// const user = {  
+//    firstName: "Talha",
+//    lastName: "Tahir",
+//    isAboy: true,
+//    age: 14,
+//    "class section": "9th-B",
+//    sayHello: function(){console.log("Hi! My Name is Talha.")},
+//    sayBye: function(){console.log("Bye! Have a Good day.")},
+// };
+// const user2 = {  
+//     firstName: "Umer",
+//     lastName: "Azeem",
+//     isAboy: false,
+//     age: 70,
+//     "class section": "Pre-9th-C",
+//     sayHello: function(){console.log("Hi! My Name is Umer Azeem.")},
+//     sayBye: () => console.log("Alright bro have a good day."),
+
+//  };
+// console.log(user.firstName);
+// console.log(user.lastName);
+// console.log(user.isAboy);
+// console.log(user.age);
+
+// console.log(user2.firstName);
+// console.log(user2.lastName);
+// console.log(user2.isAboy);
+// console.log(user2.age);
+
+// user.sayHello()
+// user.sayBye()
+// user2.sayHello()
+// user2.sayBye()
+
+// delete user.age;
+// console.log(user.age);
+// alert(user["class section"]);
+// alert(user2[isAboy]);
+
+// let user ={
+//     name: "Jhon",
+//     age: 45,
+//     password: "Nope123",
+// }
+// let key = prompt("What do you want to know about user?", "name")
+// alert(user[key]);
