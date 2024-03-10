5b5937a2fc0d267696aba1ea5c28cbe066df5631
M.Talha Tahir <hamzatalhabwp4@gmail.com>
2024-03-10 11:02:13 -0700

    Learned more about built-in Objects

--- Objects/built-in-objects.js
+++ Objects/built-in-objects.js
@@ -42,6 +42,54 @@
 
 // let a = -3.2;
 // console.log(Math.random());
-let rnd = Math.ceil(Math.random() * 100) + 1;
-console.log(rnd);
+// let rnd = Math.ceil(Math.random() * 100) + 1;
+// console.log(rnd);
 
+// Date objects
+
+
+// let todayDate = new Date(2009, 8 , 24);
+// console.log(todayDate);
+// let todayDate = new Date();
+// console.log(todayDate.getUTCHours());
+
+
+// Regular Expressions
+
+// let x = "string";
+// let pattern = /str/;
+// console.log(pattern.exec(x));
+
+
+// Number
+
+// let x = 505000/0;
+// console.log(Number.isFinite(x))
+
+// function test(x , y){
+// if(Number.isInteger(x / y)){
+//     return 'Yes Sir';
+    
+// }
+// return 'No Sir';
+// }
+
+// console.log(test(5, 0))
+// console.log(test(5, 5))
+
+
+// Array
+
+// let fruits = ["Apple", "Banana", "Grape"];
+// console.log(fruits[1]);
+
+// Error
+// let error = new Error("Something went wrong!");
+
+// function test(x, y){
+//     if(Number.isFinite(x / y)){
+//         return 'It a Number';
+//     }
+//     else return error;
+// }
+// console.log(test(5, 8))
